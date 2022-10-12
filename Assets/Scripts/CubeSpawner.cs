7ada9d02d4b33930e442f53e65d067d654a1c4d8
Artem <armastra@gmail.com>
2022-10-12 13:29:04 +0700

    Made cube prefab and spawn system

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+/// <summary>
+/// Спавнит кубы
+/// </summary>
+public class CubeSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// Префаб куба для спавна
+    /// </summary>
+    [SerializeField] 
+    private GameObject cube;
+    /// <summary>
+    /// Диапазон позиций спавна кубика по оси X (компонента x - минимальное значение, y - максимальное)
+    /// </summary>
+    [SerializeField]
+    private Vector2 xPositionRange;
+    /// <summary>
+    /// Диапазон позиций спавна кубика по оси Z (компонента x - минимальное значение, y - максимальное)
+    /// </summary>
+    [SerializeField]
+    private Vector2 zPositionRange;
+    /// <summary>
+    /// Диапазон позиций спавна кубика по оси Y (компонента x - минимальное значение, y - максимальное)
+    /// </summary>
+    [SerializeField]
+    private Vector2 yPositionRange;
+
+    /// <summary>
+    /// Спавнит куб на случайной позиции, по диапазонам, указанным в полях класса
+    /// </summary>
+    public void SpawnCube()
+    {
+        Vector3 position = new Vector3(Random.Range(xPositionRange.x, xPositionRange.y),
+            Random.Range(yPositionRange.x, yPositionRange.y),
+            Random.Range(zPositionRange.x, zPositionRange.y));
+        Instantiate(cube, position, Quaternion.identity, transform);
+    }
+}
